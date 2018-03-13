064a19787a13014f6a06d3a07e488d2444a76f7f
Jishnu <31334731+jishnukprakash98@users.noreply.github.com>
2018-03-13 16:10:50 +0530

    this is the main program

---
+++ sleep calculator.c
@@ -0,0 +1,24 @@
+//
+// this program determines how many hours you slept last night. 
+// it assumes that: 
+// you're using a 12 hour clock
+// you sleep for a maximum of 24 hours
+//
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{   // get user bedtime
+    printf("When did you go to bed last night?\n");
+    int bedtime = GetInt();
+    // determine AM or PM
+    printf("AM [1]\nor\nPM [2]\n");
+    int b = GetInt();
+    // get user wake up time
+    printf("When did you wake up this morning?\n");
+    int wakeup = GetInt();
+    // determine AM or PM
+    printf("AM [1]\nor\nPM [2]\n");
+    int w = GetInt();
+    // interpret inputs and determine how to calculate hours slept
